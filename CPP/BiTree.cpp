eb75aef6de10276e1b49b921b459afbd077989a6
翟文涛 <zhaiwentao@TaodeMacBook-Air.local>
2026-08-25 22:58:32 +0800

    modify BiTree.cpp

--- CPP/BiTree.cpp
+++ CPP/BiTree.cpp
@@ -6,12 +6,3 @@ typedef struct BiTNode
     int data;
     struct BiTNode *lchild, *rchild;
 } BiTNode, *BiTree;
-
-int main() {
-    int n = 1;
-    while(n<=10) {
-        BiTree node;
-        node->data = n;
-        n++;
-    }
-}
