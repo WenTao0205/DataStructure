61ccd64d9653c1a69a439bbdb60ed1fa896a4127
Jim <870883007@qq.com>
2026-08-26 20:47:24 +0800

    BiTree.cpp Add CreateBiTree func

--- CPP/BiTree.cpp
+++ CPP/BiTree.cpp
@@ -3,6 +3,23 @@ using namespace std;
 
 typedef struct BiTNode
 {
-    int data;
+    char data;
     struct BiTNode *lchild, *rchild;
 } BiTNode, *BiTree;
+
+void createBiTree(BiTree node) {
+    char ch;
+    cin >> ch;
+    if(ch == '#') node = NULL;
+    else {
+        node = new BiTNode;
+        node->data = ch;
+        createBiTree(node->lchild);
+        createBiTree(node->rchild);
+    }
+}
+
+int main() {
+    BiTree tree;
+    createBiTree(tree);
+}
